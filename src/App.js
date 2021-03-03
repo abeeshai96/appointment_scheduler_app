48f32621dbb20630bd8285db792504d03b10b4b8
Abeesh A I <abeeshai96@gmail.com>
2021-03-03 18:09:00 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 
 import ApptDashboard from "../src/containers/ApptDashboard/ApptDashboard";
-import FormikForm from "./components/FormikForm/FormikForm";
-import CreateAppt from "./containers/CreateAppt/CreateAppt";
-import ModalView from "../src/components/Modals/ModalView";
 
 function App() {
   return <ApptDashboard />;
