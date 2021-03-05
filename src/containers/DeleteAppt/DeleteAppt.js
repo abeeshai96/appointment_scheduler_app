73fcd9a5e8f24479c9654347a672de8b7c2d1524
Abeesh A I <abeeshai96@gmail.com>
2021-03-05 19:49:39 +0530

    New change

--- src/containers/DeleteAppt/DeleteAppt.js
+++ src/containers/DeleteAppt/DeleteAppt.js
@@ -1,8 +1,6 @@
 import { useEffect } from "react";
 import axios from "axios";
 
-import ApptDashboard from "../ApptDashboard/ApptDashboard";
-
 const DeleteAppt = (props) => {
   useEffect(() => {
     axios.delete(`/appointments/${props.data}`).then((res) => {
